25ff852a7b0b492de09e45e51ff6e19e2200302a
VladimirKaltashev <107103795+VladimirKaltashev@users.noreply.github.com>
2026-03-06 14:54:06 +0300

    Create Game.cs for main game manager

---
+++ Core/Game.cs
@@ -0,0 +1,13 @@
+// Main game manager
+using System;
+
+namespace SubwaySerf.Core
+{
+    public class Game
+    {
+        public void Start()
+        {
+            // Start game logic
+        }
+    }
+}
